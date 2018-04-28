0aa714c4c5dc5a69a40ba1bdf8b750bbcfe62aa9
Ameen Sassi <ameensassi@gmail.com>
2018-04-27 18:41:38 -0600

    Problem 4 more work done.

--- problem4.m
+++ problem4.m
@@ -1,14 +1,16 @@
-%Question 4
-c = 299792458;
+function out = problem4()
+c = 299792.458;
 
-rad = 26570000;
+rad = 26570;
 target = [0,0,6370];
 d = 0.0001;
 
-sat = [ SphericalToCartesian(rad, 0, pi/2),
-        SphericalToCartesian(rad, pi/3, 0),
-        SphericalToCartesian(rad, pi/3, 2*pi/3),
-        SphericalToCartesian(rad, pi/3, 4*pi/3) ];
+sat = zeros(4);
+
+sat(1) = SphericalToCartesian(rad, 0, pi/2);
+sat(2) = SphericalToCartesian(rad, pi/3, 0);
+sat(3) = SphericalToCartesian(rad, pi/3, 2*pi/3);
+sat(4) = SphericalToCartesian(rad, pi/3, 4*pi/3);
 
 %Random satellite positions
 %sat1 = SphericalToCartesian(rad, rand()*pi/2, rand()*pi*2);
@@ -16,8 +18,17 @@ sat = [ SphericalToCartesian(rad, 0, pi/2),
 %sat3 = SphericalToCartesian(rad, rand()*pi/2, rand()*pi*2);
 %sat4 = SphericalToCartesian(rad, rand()*pi/2, rand()*pi*2);
 
+
+%Satellite Ranges
+R1 = sqrt(((sat(1, 1))^(2))+((sat(1, 2))^(2))+((sat(1, 3)-6370)^(2))); 
+R2 = sqrt(((sat(2, 1))^(2))+((sat(2, 2))^(2))+((sat(2, 3)-6370)^(2)));
+R3 = sqrt(((sat(3, 1))^(2))+((sat(3, 2))^(2))+((sat(3, 3)-6370)^(2)));
+R4 = sqrt(((sat(4, 1))^(2))+((sat(4, 2))^(2))+((sat(4, 3)-6370)^(2)));
+
 dtnorms = zeros(100);    %The norms of the t error
 emfs = zeros(100);      %The corresponding emfs
+
+
 for i = 1:100
     t = zeros(4);
     dt = zeros(4);
@@ -26,9 +37,12 @@ for i = 1:100
         t(j) = norm(sat(j,:) - target) / c + dt(j);
     end
     
-    expectation = sat1; %replace with calculations to find position
+    expectation = problem1(0, 0, 6370, 0.0001, sat, t);
     emf = norm(expectation - target) / norm(dt) / c;
     
     dtnorms(i) = norm(dt);
     emfs(i) = emf;
+end
+
+
 end
