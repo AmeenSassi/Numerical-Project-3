94f9f2b9eb97148363bb1cc1e77f2323db9fec29
Chris <deltadaedalus@live.com>
2018-04-27 22:40:31 -0600

    Driver

--- driver.m
+++ driver.m
@@ -2,6 +2,7 @@
 format long
 
 %Question 1
+fprintf("Problem 1\n");
 pos = [ 15600   7540    20140;
         18760   2750    18610;
         17610   14630   13480;
@@ -10,7 +11,8 @@ t = [0.07074, 0.07220, 0.07690, 0.07242];
 ans1 = problem1(0, 0, 6370, 0, pos, t);
 fprintf("Calculated Position: %d, %d, %d\n", ans1(1), ans1(2), ans1(3));
 fprintf("Calculated d: %d\n", ans1(4));
-disp(ans1);
+
+pause;
 
 %Question 2
 
